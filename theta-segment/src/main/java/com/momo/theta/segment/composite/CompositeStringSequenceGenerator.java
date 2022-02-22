5b549e29297df011260fc6bcaf277167a2b0ab40
zBo1997 <53822786+zBo1997@users.noreply.github.com>
2022-02-22 18:57:41 +0800

    {add commit & init README.md}

--- theta-segment/src/main/java/com/momo/theta/segment/composite/CompositeStringSequenceGenerator.java
+++ theta-segment/src/main/java/com/momo/theta/segment/composite/CompositeStringSequenceGenerator.java
@@ -14,6 +14,10 @@ public class CompositeStringSequenceGenerator implements ThetaSegment {
 
     private List<ThetaSegment> ThetaSegments = new ArrayList<>();
 
+    /**
+     * 通过所有已经注册了Segment使用StringBuilder来生成对应德Sequence
+     * @return
+     */
     @Override
     public String getSequence() {
         StringBuilder sequenceBuilder = new StringBuilder();
@@ -24,6 +28,11 @@ public class CompositeStringSequenceGenerator implements ThetaSegment {
         return sequenceBuilder.toString();
     }
 
+    /**
+     * 支持多个可变参数的组合
+     * @param argsMap 多个可变参数
+     * @return
+     */
     public String getStringSequence(Map<String, String> argsMap) {
         StringBuilder sequenceBuilder = new StringBuilder();
         for (ThetaSegment ThetaSegment : ThetaSegments) {
@@ -38,6 +47,12 @@ public class CompositeStringSequenceGenerator implements ThetaSegment {
         return sequenceBuilder.toString();
     }
 
+    /**
+     * 基于父SegmentNum和可变参数生成序列号
+     * @param parentSegmentNum 父Segment
+     * @param argsMap 可变参数 可以通过 <p>#{code1}#{code2}#{code3}</p> 的这种方式进行组合
+     * @return
+     */
     public String getStringSequenceByParentSegment(int parentSegmentNum, Map<String, String> argsMap) {
         StringBuilder sequenceBuilder = new StringBuilder();
         int i = 0;
@@ -56,6 +71,11 @@ public class CompositeStringSequenceGenerator implements ThetaSegment {
         return sequenceBuilder.toString();
     }
 
+    /**
+     * 根据父Segment生成序列号
+     * @param parentSegmentNum 父Segment
+     * @return
+     */
     public String getStringSequenceByParentSegment(int parentSegmentNum) {
         StringBuilder sequenceBuilder = new StringBuilder();
         int i = 0;
@@ -69,10 +89,18 @@ public class CompositeStringSequenceGenerator implements ThetaSegment {
         return sequenceBuilder.toString();
     }
 
+    /**
+     * 设置所有的生成Segment
+     * @param ThetaSegments
+     */
     public void setThetaSegments(List<ThetaSegment> ThetaSegments) {
         this.ThetaSegments = ThetaSegments;
     }
 
+    /**
+     * 当前的类型
+     * @return
+     */
     @Override
     public String getType() {
         return "compositeString";
@@ -81,8 +109,8 @@ public class CompositeStringSequenceGenerator implements ThetaSegment {
     /**
      * 初始化组合配置，全放在一个有序的线性表中
      *
-     * @param segmentConfig
-     * @throws Exception
+     * @param segmentConfig segment配置
+     * @throws Exception 异常信息
      */
     @Override
     public void init(SegmentConfig segmentConfig) throws Exception {
