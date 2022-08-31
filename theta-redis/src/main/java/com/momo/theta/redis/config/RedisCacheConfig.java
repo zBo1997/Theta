44f8a1281fb4ed460c3c0d0181b34152bc0e81fa
zBo1997 <53822786+zBo1997@users.noreply.github.com>
2022-09-01 00:06:15 +0800

    redis 创建
    [redis init]

---
+++ theta-redis/src/main/java/com/momo/theta/redis/config/RedisCacheConfig.java
@@ -0,0 +1,54 @@
+package com.momo.theta.redis.config;
+
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+import com.fasterxml.jackson.annotation.PropertyAccessor;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.jsontype.impl.LaissezFaireSubTypeValidator;
+import com.momo.theta.redis.properties.RedisCacheProperties;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.CachingConfigurerSupport;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
+
+import java.time.Duration;
+
+@Slf4j
+@AllArgsConstructor
+@Configuration
+@EnableCaching
+@EnableConfigurationProperties(RedisCacheProperties.class)
+@ConditionalOnProperty(prefix = "spring.redis.cache", name = "enable", havingValue = "true")
+public class RedisCacheConfig extends CachingConfigurerSupport {
+
+
+    private RedisCacheProperties cacheProperties;
+
+
+    @Bean
+    public CacheManager cacheManager(RedisConnectionFactory redisConnectionFactory) {
+        RedisCacheConfiguration configuration = RedisCacheConfiguration.defaultCacheConfig();
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.ANY);
+        mapper.activateDefaultTyping(LaissezFaireSubTypeValidator.instance, ObjectMapper.DefaultTyping.NON_FINAL, JsonTypeInfo.As.PROPERTY);
+        Jackson2JsonRedisSerializer<Object> jackson2JsonRedisSerializer = new Jackson2JsonRedisSerializer<>(Object.class);
+        jackson2JsonRedisSerializer.setObjectMapper(mapper);
+        RedisCacheConfiguration config = configuration
+                .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(jackson2JsonRedisSerializer))
+                .entryTtl(Duration.ofMinutes(cacheProperties.getTtl()))
+                .computePrefixWith(cacheName -> cacheProperties.getPrefix() + "_" + cacheName + "::");
+        return RedisCacheManager.builder(redisConnectionFactory).cacheDefaults(config).build();
+    }
+
+
+}
---
+++ theta-redis/src/main/java/com/momo/theta/redis/properties/RedisCacheProperties.java
@@ -0,0 +1,12 @@
+package com.momo.theta.redis.properties;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@Data
+@ConfigurationProperties(prefix = "spring.redis.cache")
+public class RedisCacheProperties {
+    private String prefix = "";
+    private long ttl = 30L * 24 * 60;
+
+}
