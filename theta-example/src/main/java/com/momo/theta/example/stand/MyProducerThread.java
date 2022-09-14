261c9ecf134ec31866941047f9d4e408a204ea42
zBo1997 <53822786+zBo1997@users.noreply.github.com>
2022-09-14 23:44:48 +0800

    [初始化 示例 项目] (#11)
    * [添加示例项目]
    
    * 添加docker-compose
    
    [添加项目所需要使用的中间件 docker-compose ]
    
    * Update ThetaSegmentTest.java
    
    [多线程测试 和使用]
    
    * Create application.yaml
    
    [测试多线程下生成sequence]
    
    * Update README.md
    
    [reademe 修改]
    
    * [修改docker_componse]
    
    [修改docker_componse]
    
    * [rocketMq 实例代码]
    
    [rocketMq 实例代码]

---
+++ theta-example/src/main/java/com/momo/theta/example/stand/MyProducerThread.java
@@ -0,0 +1,46 @@
+package com.momo.theta.example.stand;
+
+
+import com.momo.theta.mq.queue.StandDisruptorQueue;
+
+import java.util.Calendar;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class MyProducerThread implements Runnable {
+    private String name;
+    private StandDisruptorQueue disruptorQueue;
+    private volatile boolean flag = true;
+    private static AtomicInteger count = new AtomicInteger();
+ 
+    public MyProducerThread(String name, StandDisruptorQueue disruptorQueue) {
+        this.name = name;
+        this.disruptorQueue = disruptorQueue;
+    }
+ 
+    @Override
+    public void run() {
+        try {
+            System.out.println(now() + this.name + "：线程启动。");
+            while (flag) {
+                String data = count.incrementAndGet()+"";
+                // 将数据存入队列中
+                disruptorQueue.add(data);
+                System.out.println(now() + this.name + "：存入" + data + "到队列中。");
+            }
+        } catch (Exception e) {
+ 
+        } finally {
+            System.out.println(now() + this.name + "：退出线程。");
+        }
+    }
+ 
+    public void stopThread() {
+        this.flag = false;
+    }
+ 
+    // 获取当前时间（分:秒）
+    public String now() {
+        Calendar now = Calendar.getInstance();
+        return "[" + now.get(Calendar.MINUTE) + ":" + now.get(Calendar.SECOND) + "] ";
+    }
+}
