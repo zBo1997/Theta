99f3c004aac4e3064ec059ace53aab424ca18610
zBo1997 <53822786+zBo1997@users.noreply.github.com>
2022-09-14 11:53:55 +0800

    Update ThetaSegmentTest.java
    [多线程测试 和使用]

--- theta-example/src/test/java/com/momo/example/segment/ThetaSegmentTest.java
+++ theta-example/src/test/java/com/momo/example/segment/ThetaSegmentTest.java
@@ -14,7 +14,16 @@ public class ThetaSegmentTest {
 
     @Test
     public void testSegment() {
-
+        // 多线程生成sequence
+        for (int i = 0; i < 100; i++) {
+            new Thread(() -> {
+                //指定
+                Sequence sequence = new Sequence("myCompositeString");
+                HashMap<String, String> args = new HashMap<>();
+                //args.put("customerString", "100");
+                System.out.println(sequence.getSequence(args));
+            }).start();
+        }
         log.info("I am test");
 
     }
