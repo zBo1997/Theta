cc6c6540da11ae1f3bad1a21ddbf5afb348b4df6
zhubo <zhubo@vv.cn>
2023-10-20 15:26:44 +0800

    [认证服务以所需依赖]

--- theta-ouath2-server/ouath-server/src/main/java/com/momo/theta/ThetaAuthSpringBootApplication.java
+++ theta-ouath2-server/ouath-server/src/main/java/com/momo/theta/ThetaAuthSpringBootApplication.java
@@ -1,5 +1,18 @@
 package com.momo.theta;
 
+
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cloud.client.SpringCloudApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.scheduling.annotation.EnableAsync;
+
+@EnableDiscoveryClient
+@EnableAspectJAutoProxy(proxyTargetClass = true, exposeProxy = true)
+@EnableCaching
+@SpringCloudApplication
+@EnableFeignClients(AppConstant.BASE_PACKAGES)
+@EnableAsync
 public class ThetaAuthSpringBootApplication {
 
 
